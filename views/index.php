0e802f3baf6f7fbd10fcc1ea8fb0bbc4d432b535
Isabelle Hondo <isefshondo@gmail.com>
2023-04-07 13:10:24 -0300

    Feat: Header components done

---
+++ views/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Point here the rest of the meta tags -->
+    <title>Petshop Dogin's</title>
+    <link rel="stylesheet" href="./assets/styles/styles.css">
+  </head>
+  <body>
+    <input type="search" name="searchProdutc" id="searchProduct" placeholder = "O que o seu pet precisa?">
+    <a href="#">
+      <span class = "likeIcon"></span>
+      Mais comprados
+    </a>
+    <a href="#">
+      <span class = "cartIcon"></span>
+      Meu carrinho
+    </a>
+    <button class = "btnSeller">Sou vendedor</button>
+    <button class = "btnLoginSignin">Entrar/Cadastrar</button>
+    <script src="./assets/js/main.js"></script>
+  </body>
+</html>
