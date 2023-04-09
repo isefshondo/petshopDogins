0c0027e3e46173ff519800ee24d5b52bf0a70a2a
Isabelle Hondo <isefshondo@gmail.com>
2023-04-09 17:49:47 -0300

    Fixed: Display of pets categories

--- views/index.php
+++ views/index.php
@@ -3,93 +3,97 @@ include("./blades/header.php");
 include("./partials/header.php");
 include("./components/sectionHeader.php");
 ?>
-<!-- <main>
-      <?php include("./components/couponNotice.php") ?>
-      <section id="imgs-carousel">
-        <div id = "imgs-slider" class = "imgs-slider">
-        <?php
-        $imgsCarousel = array(
-          array(
-            "imgName" => "img-carousel-01.png",
-            "alt" => "Seu dog AUUdacioso!"
-          ),
-        );
-        foreach ($imgsCarousel as $img) {
-          echo "<img src='./assets/imgs/" . $img["imgName"] . "' alt='" . $img["alt"] . "'>";
-        }
-        ?>
-        </div>
-      </section> -->
-      <section id="firstShowProducts">
-        <?php
-        createHeaderSection("Produtos AUUdaciosos", true);
-        ?>
-        <ul class="productsWrapper">
-          <li class="productCard">
-            <figure class="productImg">
-              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
-            </figure>
-            <hgroup class="productDesc">
-              <h2>Nome do produto</h2>
-              <h3>Marca: Marca do produto</h3>
-            </hgroup>
-            <hgroup class="productPrice">
-              <h3>R&#36; Preço produto</h3>
-              <h2>R&#36; Preço produto</h2>
-            </hgroup>
-          </li>
-        </ul>
+<main>
+  <?php include("./components/couponNotice.php") ?>
+  <!-- <section id="imgs-carousel">
+    <div id="imgs-slider" class="imgs-slider">
+      <?php
+      $imgsCarousel = array(
+        array(
+          "imgName" => "img-carousel-01.png",
+          "alt" => "Seu dog AUUdacioso!"
+        ),
+      );
+      foreach ($imgsCarousel as $img) {
+        echo "<img src='./assets/imgs/" . $img["imgName"] . "' alt='" . $img["alt"] . "'>";
+      }
+      ?>
+    </div>
+  </section> -->
+  <section id="firstShowProducts">
+    <?php
+    createHeaderSection("Produtos AUUdaciosos", true);
+    ?>
+    <ul class="productsWrapper">
+      <li class="productCard">
+        <figure class="productImg">
+          <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+        </figure>
+        <hgroup class="productDesc">
+          <h2>Nome do produto</h2>
+          <h3>Marca: Marca do produto</h3>
+        </hgroup>
+        <hgroup class="productPrice">
+          <h3>R&#36; Preço produto</h3>
+          <h2>R&#36; Preço produto</h2>
+        </hgroup>
+      </li>
+    </ul>
+  </section>
+  <section id="productsCategories">
+    <?php
+    createHeaderSection("Categoria de produtos", false);
+    ?>
+    <ul class="categories">
+      <li>
+        <img src="./assets/imgs/accessories.svg" alt="Acessorios">
+        <h1>Nome da categoria</h1>
+      </li>
+    </ul>
+  </section>
+  <section id="secondShowProducts">
+    <?php
+    createHeaderSection("Produtos AUUdaciosos", true);
+    ?>
+    <ul class="productsWrapper">
+      <li class="productCard">
+        <figure class="productImg">
+          <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+        </figure>
+        <hgroup class="productDesc">
+          <h2>Bicho de pelúcia</h2>
+          <h3>Marca: Marca do produto</h3>
+        </hgroup>
+        <hgroup class="productPrice">
+          <h3>R&#36; Preço produto</h3>
+          <h2>R&#36; Preço produto</h2>
+        </hgroup>
+      </li>
+    </ul>
+  </section>
+  <section id="searchByPets">
+    <?php
+    createHeaderSection("Busque o melhor para o seu pet", false);
+    ?>
+    <div class="petsCategories">
+      <section class="firstGroupCategories">
+        <figure class="dog-category">
+          <h1>Cachorros</h1>
+        </figure>
+        <figure class="cat-category">
+          <h1>Gatos</h1>
+        </figure>
       </section>
-      <!--<section id="productsCategories">
-        <?php
-        createHeaderSection("Categoria de produtos", false);
-        ?>
-        <ul class = "categories">
-          <li>
-            <img src="./assets/imgs/accessories.svg" alt="Acessorios">
-            <h1>Nome da categoria</h1>
-          </li>
-        </ul>
+      <section class="secondGroupCategories">
+        <figure class="others-category">
+          <h1>Outros pets</h1>
+        </figure>
+        <figure class="puppies-category">
+          <h1>Filhotes</h1>
+        </figure>
       </section>
-      <section id="secondShowProducts">
-        <?php
-        createHeaderSection("Produtos AUUdaciosos", true);
-        ?>
-        <ul class="productsWrapper">
-          <li class="productCard">
-            <figure class="productImg">
-              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
-            </figure>
-            <hgroup class="productDesc">
-              <h2>Nome do produto</h2>
-              <h3>Marca: Marca do produto</h3>
-            </hgroup>
-            <hgroup class="productPrice">
-              <h3>R&#36; Preço produto</h3>
-              <h2>R&#36; Preço produto</h2>
-            </hgroup>
-          </li>
-        </ul>
-      </section>
-      <section id="searchByPets">
-        <?php
-        createHeaderSection("Busque o melhor para o seu pet", false);
-        ?>
-        <div class="petsCategories">
-          <figure class="dog-category">
-            <h1>Cachorros</h1>
-          </figure>
-          <figure class="cat-category">
-            <h1>Gatos</h1>
-          </figure>
-          <figure class="others-category">
-            <h1>Outros pets</h1>
-          </figure>
-          <figure class="puppies-category">
-            <h1>Filhotes</h1>
-          </figure>
-        </div>
-      </section>
-    </main> -->
+    </div>
+  </section>
+</main>
 <?php include("./partials/footer.php") ?>
 <?php include("./blades/footer.php") ?>
