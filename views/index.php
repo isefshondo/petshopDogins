409a7e2124e750139b2de88c871bf7ffceac71de
Isabelle Hondo <isefshondo@gmail.com>
2023-04-09 14:41:45 -0300

    Fixed: Products cards now has the right size

--- views/index.php
+++ views/index.php
@@ -1,31 +1,82 @@
-<?php 
-  include("./blades/header.php");
-  include("./partials/header.php");
-  include("./components/sectionHeader.php");
+<?php
+include("./blades/header.php");
+include("./partials/header.php");
+include("./components/sectionHeader.php");
 ?>
-    <main>
-      <?php include("./components/couponNotice.php")?>
+<!-- <main>
+      <?php include("./components/couponNotice.php") ?>
       <section id="imgs-carousel">
         <div id = "imgs-slider" class = "imgs-slider">
         <?php
-          $imgsCarousel = array(
-            array(
-              "imgName" => "img-carousel-01.png",
-              "alt" => "Seu dog AUUdacioso!"
-            ),
-          );
-          foreach($imgsCarousel as $img) {
-            echo "<img src='./assets/imgs/".$img["imgName"]."' alt='".$img["alt"]."'>";
-          }
+        $imgsCarousel = array(
+          array(
+            "imgName" => "img-carousel-01.png",
+            "alt" => "Seu dog AUUdacioso!"
+          ),
+        );
+        foreach ($imgsCarousel as $img) {
+          echo "<img src='./assets/imgs/" . $img["imgName"] . "' alt='" . $img["alt"] . "'>";
+        }
         ?>
         </div>
-      </section>
+      </section> -->
       <section id="firstShowProducts">
         <?php
-          createHeaderSection("Produtos AUUdaciosos", true);
-          ?>
-        <!-- TODO: Fix the products card issue. The same issue of the Weekly Planner -->
+        createHeaderSection("Produtos AUUdaciosos", true);
+        ?>
         <ul class="productsWrapper">
+          <li class="productCard">
+            <figure class="productImg">
+              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+            </figure>
+            <hgroup class="productDesc">
+              <h2>Nome do produto</h2>
+              <h3>Marca: Marca do produto</h3>
+            </hgroup>
+            <hgroup class="productPrice">
+              <h3>R&#36; Preço produto</h3>
+              <h2>R&#36; Preço produto</h2>
+            </hgroup>
+          </li>
+          <li class="productCard">
+            <figure class="productImg">
+              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+            </figure>
+            <hgroup class="productDesc">
+              <h2>Nome do produto</h2>
+              <h3>Marca: Marca do produto</h3>
+            </hgroup>
+            <hgroup class="productPrice">
+              <h3>R&#36; Preço produto</h3>
+              <h2>R&#36; Preço produto</h2>
+            </hgroup>
+          </li>
+          <li class="productCard">
+            <figure class="productImg">
+              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+            </figure>
+            <hgroup class="productDesc">
+              <h2>Nome do produto</h2>
+              <h3>Marca: Marca do produto</h3>
+            </hgroup>
+            <hgroup class="productPrice">
+              <h3>R&#36; Preço produto</h3>
+              <h2>R&#36; Preço produto</h2>
+            </hgroup>
+          </li>
+          <li class="productCard">
+            <figure class="productImg">
+              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+            </figure>
+            <hgroup class="productDesc">
+              <h2>Nome do produto</h2>
+              <h3>Marca: Marca do produto</h3>
+            </hgroup>
+            <hgroup class="productPrice">
+              <h3>R&#36; Preço produto</h3>
+              <h2>R&#36; Preço produto</h2>
+            </hgroup>
+          </li>
           <li class="productCard">
             <figure class="productImg">
               <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
@@ -41,9 +92,9 @@
           </li>
         </ul>
       </section>
-      <section id="productsCategories">
+      <!--<section id="productsCategories">
         <?php
-          createHeaderSection("Categoria de produtos", false);
+        createHeaderSection("Categoria de produtos", false);
         ?>
         <ul class = "categories">
           <li>
@@ -54,7 +105,7 @@
       </section>
       <section id="secondShowProducts">
         <?php
-          createHeaderSection("Produtos AUUdaciosos", true);
+        createHeaderSection("Produtos AUUdaciosos", true);
         ?>
         <ul class="productsWrapper">
           <li class="productCard">
@@ -74,7 +125,7 @@
       </section>
       <section id="searchByPets">
         <?php
-          createHeaderSection("Busque o melhor para o seu pet", false);
+        createHeaderSection("Busque o melhor para o seu pet", false);
         ?>
         <div class="petsCategories">
           <figure class="dog-category">
@@ -91,6 +142,6 @@
           </figure>
         </div>
       </section>
-    </main>
-    <?php include("./partials/footer.php")?>
-<?php include("./blades/footer.php")?>
+    </main> -->
+<?php include("./partials/footer.php") ?>
+<?php include("./blades/footer.php") ?>
