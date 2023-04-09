c711ecacd6c8b839578acdcc628c57632558ab15
Isabelle Hondo <isefshondo@gmail.com>
2023-04-09 00:10:53 -0300

    Chore: Pets categories display is on work

--- views/index.php
+++ views/index.php
@@ -23,25 +23,23 @@
       <section id="firstShowProducts">
         <?php
           createHeaderSection("Produtos AUUdaciosos", true);
-        ?>
-        <main>
-          <!-- TODO: Fix the products card issue. The same issue of the Weekly Planner -->
-          <ul class="productsWrapper">
-            <li class="productCard">
-              <figure class="productImg">
-                <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
-              </figure>
-              <hgroup class="productDesc">
-                <h2>Nome do produto</h2>
-                <h3>Marca: Marca do produto</h3>
-              </hgroup>
-              <hgroup class="productPrice">
-                <h3>R&#36; Preço produto</h3>
-                <h2>R&#36; Preço produto</h2>
-              </hgroup>
-            </li>
-          </ul>
-        </main>
+          ?>
+        <!-- TODO: Fix the products card issue. The same issue of the Weekly Planner -->
+        <ul class="productsWrapper">
+          <li class="productCard">
+            <figure class="productImg">
+              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+            </figure>
+            <hgroup class="productDesc">
+              <h2>Nome do produto</h2>
+              <h3>Marca: Marca do produto</h3>
+            </hgroup>
+            <hgroup class="productPrice">
+              <h3>R&#36; Preço produto</h3>
+              <h2>R&#36; Preço produto</h2>
+            </hgroup>
+          </li>
+        </ul>
       </section>
       <section id="productsCategories">
         <?php
@@ -58,6 +56,40 @@
         <?php
           createHeaderSection("Produtos AUUdaciosos", true);
         ?>
+        <ul class="productsWrapper">
+          <li class="productCard">
+            <figure class="productImg">
+              <img src="./assets/imgs/firstToy.png" alt="Produtos AUUdaciosos">
+            </figure>
+            <hgroup class="productDesc">
+              <h2>Nome do produto</h2>
+              <h3>Marca: Marca do produto</h3>
+            </hgroup>
+            <hgroup class="productPrice">
+              <h3>R&#36; Preço produto</h3>
+              <h2>R&#36; Preço produto</h2>
+            </hgroup>
+          </li>
+        </ul>
+      </section>
+      <section id="searchByPets">
+        <?php
+          createHeaderSection("Busque o melhor para o seu pet", false);
+        ?>
+        <div class="petsCategories">
+          <figure class="dog-category">
+            <h1>Cachorros</h1>
+          </figure>
+          <figure class="cat-category">
+            <h1>Gatos</h1>
+          </figure>
+          <figure class="others-category">
+            <h1>Outros pets</h1>
+          </figure>
+          <figure class="puppies-category">
+            <h1>Filhotes</h1>
+          </figure>
+        </div>
       </section>
     </main>
     <?php include("./partials/footer.php")?>
