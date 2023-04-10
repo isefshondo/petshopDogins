dcb4e534fac12f618f70b3f77416fe8acc973bc7
Isabelle Hondo <isefshondo@gmail.com>
2023-04-10 11:52:00 -0300

    Chore: Structure of view product page

---
+++ views/viewProduct.php
@@ -0,0 +1,16 @@
+<?php
+include("./blades/header.php");
+include("./partials/header.php");
+include("./components/sectionHeader.php");
+?>
+<main>
+  <?php include("./components/couponNotice.php") ?>
+  <div class = "detailsHolder">
+    <section id="purchaseDetails"></section>
+    <section id="finishPurchase"></section>
+  </div>
+</main>
+<?php
+include("./partials/footer.php");
+include("./blades/footer.php");
+?>
