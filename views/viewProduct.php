7e88e5741e826096542dff7842d83d31d8363407
Isabelle Hondo <isefshondo@gmail.com>
2023-04-11 09:33:04 -0300

    Chore: Working on view price card

--- views/viewProduct.php
+++ views/viewProduct.php
@@ -11,12 +11,14 @@ include("./components/sectionHeader.php");
     <section id = "viewPriceDetails">
       <form class = "viewPriceCard">
         <header class = "chooseBuyFrequently">
-          <hgroup>
-            <h3 class = "originalPrice">R$ 163,90</h3>
-            <h5 class = "getDiscountPrice">-5%</h5>
-            <span class = "disclaimerIcon"></span>
-          </hgroup>
-          <h1>R$ 153,91</h1>
+          <div>
+            <hgroup>
+              <h3 class = "originalPrice">R$ 163,90</h3>
+              <h5 class = "getDiscountPrice">-5%</h5>
+              <span class = "disclaimerIcon"></span>
+            </hgroup>
+            <h1>R$ 153,91</h1>
+          </div>
           <button class = "btnBuyFrequently">
             <span class = "checkMarkIcon"></span>
             Comprar com recorrência por R&#36; 53,91
@@ -25,6 +27,13 @@ include("./components/sectionHeader.php");
             Se compras com recorrência ganhas desconto considerável em tua compra. 
           </p>
         </header>
+        <div class = "chooseBuy">
+          <h1>R&#36; 163,90</h1>
+          <button class = "btnBuyProduct">
+            <span class = "cartIcon"></span>
+            Comprar por R&#36; 163,90
+          </button>
+        </div>
       </form>
     </section>
   </div>
