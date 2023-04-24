b92e2c939f829d5cdd07dc27c2cf0b376c532bd2
aasjunior <adeldivo@hotmail.com>
2023-04-24 20:22:30 -0300

    login admin

--- cms/models/login.php
+++ cms/models/login.php
@@ -1,4 +1,14 @@
 <?php
+    if($_POST['usuario'] == 'admin' && $_POST['senha'] == 'admin'){
+        // Criar uma sessão para o usuário
+        session_start();
+        $_SESSION['username'] = 'admin';
+        echo json_encode(['status' => 'success', 'redirect' => '../views/homePage.php']);
+    }else{
+        // Retornar uma resposta de erro
+        echo json_encode(['status' => 'error', 'message' => 'Usuário ou senha inválidos.']);
+    }
+    /*
     include("../../config/conn.php");
 
     $collection = $db->users;
@@ -15,5 +25,6 @@
         // Retornar uma resposta de erro
         echo json_encode(['status' => 'error', 'message' => 'Usuário ou senha inválidos.']);
     }
+    */
 
 ?>
